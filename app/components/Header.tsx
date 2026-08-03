1d69ad75e6c9eb65a638f9ba48a8c2db987fdf3d
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-03 14:53:03 +0330

    add : custom font's added

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,9 @@
 export default function Header() {
     return (
         <header className="fixed w-[80%] h-20 top-6 rounded-3xl shadow-sm backdrop-blur-sm bg-main">
-            asdasd
+            <button className="font-yekan">
+                
+            </button>
         </header>
     )    
 }
