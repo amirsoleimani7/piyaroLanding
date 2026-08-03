2c5e2795075fe56a14cd27437224923e94e2b468
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-03 13:41:10 +0330

    add : nextjs config

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-red-100">{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
 
 export default function Home() {
-  return <div>main page</div>;
+  return (<></>);
 }
-
-
-
