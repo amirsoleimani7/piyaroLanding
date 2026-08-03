bb86c63b4cad68191f087f507d0b6ba8ad5ae773
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-03 16:39:26 +0330

    add : header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,21 +1,34 @@
 import { IoIosDownload } from "react-icons/io";
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="fixed w-[80%] top-6 rounded-3xl inset-shadow-2xs backdrop-blur-sm flex items-center px-6 py-4 border max-md:w-full max-md:top-0 max-md:rounded-b-3xl max-md:rounded-t-none">
+    <header className="fixed w-[80%] top-6 rounded-2xl inset-shadow-black backdrop-blur-sm flex justify-around items-center px-5 py-3 shadow-xl  max-md:w-full max-md:top-0 max-md:rounded-b-2xl max-md:rounded-t-none">
+      <section className="flex h-full gap-3 items-center ">
+        <Image
+          src="/logo/bg-removed.png"
+          width={40}
+          height={40}
+          alt="Picture of the Company Logo"
+        />
+        <div className="h-8 w-1 border-none rounded-full bg-main"></div>
+
+        <span className="font-lemonade text-xl ">پیارو گلد</span>
+      </section>
+
+      {/* <section>main section</section> */}
+
       <section className="mr-auto ml-0 flex gap-3">
         <button className="p-2 rounded-xl cursor-pointer">
-          <span className="font-yekan flex gap-0.5 font-bold">
+          <span className="font-yekan flex gap-0.5 text-gray-700">
             <IoIosDownload />
             دانلود اپلیکیشن{" "}
           </span>
         </button>
-        <button className="p-3 rounded-xl bg-main cursor-pointer">
+        <button className="px-3 py-2 rounded-xl bg-main cursor-pointer">
           <span className="font-yekan font-bold">ورود | ثبت‌نام</span>
         </button>
       </section>
-
-      <section></section>
     </header>
   );
 }
