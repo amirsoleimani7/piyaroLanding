3dd98e5ef57ebd0c6f6fbaa6b10b8f8a9b2d01b7
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-06 14:13:37 +0330

    add : Autoplay pluguin added to Carousel

--- app/components/businessLicense/Licenses.tsx
+++ app/components/businessLicense/Licenses.tsx
@@ -1,31 +1,4 @@
-// import {
-//   Carousel,
-//   CarouselContent,
-//   CarouselItem,
-//   CarouselNext,
-//   CarouselPrevious,
-// } from "@/components/ui/carousel";
-
-// export default function Licenses() {
-//   return (
-//     <div className="w-full mt-10 h-100 bg-red-100">
-//       <Carousel className="w-full">
-//         <CarouselContent>
-//           <CarouselItem>
-//             <div className="w-[200px] h-[200px] bg-green-500">somehin</div>
-//           </CarouselItem>
-//           <CarouselItem>
-//             <div className="w-[200px] h-[200px] bg-green-500">somehin</div>
-//           </CarouselItem>
-//           <CarouselItem>
-//             <div className="w-10 h-10 bg-green-500">somehin</div>
-//           </CarouselItem>
-//         </CarouselContent>
-//       </Carousel>
-//     </div>
-//   );
-// }
-
+"use client"
 import {
   Carousel,
   CarouselContent,
@@ -33,9 +6,9 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import Autoplay from 'embla-carousel-autoplay'
 
 import { license, LicenseType } from "../../../public/data/LicensesData";
-
 
 export default function Licenses() {
   return (
@@ -43,18 +16,21 @@ export default function Licenses() {
       opts={{
         align: "start",
       }}
-      className="w-full max-w-3xl bg-red-500 "
+      plugins={[
+        Autoplay({delay : 1000})
+      ]}     
+      className="w-full max-w-3xl bg-red-500 mt-auto"
     >
       <CarouselContent>
         {license.map((l: LicenseType, index: number) => (
-          <CarouselItem key={index} className="basis-1/4">
+          <CarouselItem key={index} className="basis-1/3">
             <div className="p-1">
               <div className="relative aspect-square overflow-hidden border-0 p-0">
                 <img
                   src={l.URL}
                   alt={`License ${index + 1}`}
-                  width={400}
-                  height={400}
+                  width={200}
+                  height={200}
                   className="h-full w-full object-cover"
                 />
               </div>
