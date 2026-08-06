47af5b2c0577f3ab834f1909869ecccc441169dc
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-06 16:24:51 +0330

    add : title to licenses added

--- app/components/businessLicense/Licenses.tsx
+++ app/components/businessLicense/Licenses.tsx
@@ -6,41 +6,46 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/autoplay";
+import { Rock_3D } from "next/font/google";
 
 export default () => {
   return (
-    <Swiper
-      modules={[Autoplay]}
-      spaceBetween={5}
-      loop
-      autoplay={{
-        delay: 1000,
-        disableOnInteraction: false,
-      }}
-      
-      slidesPerView={5}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
-      className="w-full max-w-4xl  mb-20 mt-auto shadow-2xl p-5 rounded-2xl"
-    >
-      {license.map((l: LicenseType, index: number) => (
-        <SwiperSlide
-          key={index}
-          className="flex items-center justify-center rounded-2xl w-10 h-10 bg-red-700"
-        >
-          <div className="p-1">
-            <div className="relative aspect-square overflow-hidden border-0 p-0 flex items-center justify-center">
-              <img
-                src={l.URL}
-                alt={`License ${index + 1}`}
-                width={10}
-                height={10}
-                className="h-full w-full object-cover"
-              />
+    <div className="flex flex-col items-center mb-20 mt-auto ">
+      <div className="rounded-2xl border-none px-3 py-1 outline-1 outline-gray-300 shadow-2xl">
+        <h1 className="font-yekan text-xl font-bold">مجوزها</h1>
+      </div>
+      <Swiper
+        modules={[Autoplay]}
+        spaceBetween={10}
+        loop
+        autoplay={{
+          delay: 1000,
+          disableOnInteraction: false,
+        }}
+        slidesPerView={5}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        className="w-full max-w-4xl  shadow-xl p-5 rounded-2xl px-10"
+      >
+        {license.map((l: LicenseType, index: number) => (
+          <SwiperSlide
+            key={index}
+            className="flex items-center justify-center rounded-2xl w-10 h-10 "
+          >
+            <div className="p-1">
+              <div className="relative aspect-square overflow-hidden border-0 p-0 flex items-center justify-center">
+                <img
+                  src={l.URL}
+                  alt={`License ${index + 1}`}
+                  width={10}
+                  height={10}
+                  className="w-[50%] h-[50%] object-cover"
+                />
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
