def7eb2b02be7503c66b4928dc0aa7fff8cfba0e
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-07 20:46:47 +0330

    add : fixed devider on last items

--- app/components/FAQ/Faq.tsx
+++ app/components/FAQ/Faq.tsx
@@ -1,20 +1,23 @@
 import { FaqItem } from "./faqItem";
 import { faqItemsdata, faqType } from "../../../public/data/FAQdata";
 
-
 export default function Faq() {
   console.log(faqItemsdata);
 
   return (
     <div className="flex flex-col  mt-auto mb-10 w-[60%] rounded-xl ">
-      <h1 className="font-yekan font-bold text-center text-4xl text-gray-600">
+      <h1 className="font-yekan font-bold text-center text-3xl mb-10">
         سوالات متداول
       </h1>
       <section className="w-full flex flex-col">
         {faqItemsdata.map((f: faqType, index: number) => (
           <div className="flex flex-col mb-6" key={index}>
             <FaqItem question={f.question} awnser={f.awnser} />
-            <div className="w-full  border-b border-gray-300"></div>
+            {faqItemsdata[faqItemsdata.length - 1] === f ? (
+              <></>
+            ) : (
+              <div className="w-full  border-b border-gray-300"></div>
+            )}
           </div>
         ))}
       </section>
