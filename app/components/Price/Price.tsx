b030d68e3a40646bffabcb387640979d862a7de3
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-09 23:23:59 +0330

    add : repsonsive layout

--- app/components/Price/Price.tsx
+++ app/components/Price/Price.tsx
@@ -1,10 +1,10 @@
 export default function Price() {
   return (
-    <section className="w-[70%] max-md:w-[95%] rounded-xl overflow-hidden h-200 bg-red-500 flex">
-      <div className="w-1/2 bg-emerald-400">
+    <section className="w-[70%] max-md:w-[95%] rounded-xl overflow-hidden h-200 bg-red-500 flex max-lg:flex-col">
+      <div className="w-1/2 h-full bg-emerald-400 max-lg:w-full">
         
       </div>
-      <div className="w-1/2 bg-yellow-400">
+      <div className="w-1/2 h-full bg-yellow-400 max-lg:w-full">
         something2
       </div>
     </section>
