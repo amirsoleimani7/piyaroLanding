a4cfe6e9f1898d86b5765856e4597ad21a44c7f0
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-11 17:13:08 +0330

    add : header and desc for Buy and sell section added

--- app/components/Price/Price.tsx
+++ app/components/Price/Price.tsx
@@ -8,7 +8,6 @@ export default function Price() {
 
   return (
     <section className="w-[70%] max-md:w-[95%] rounded-md h-100 flex gap-5 max-lg:flex-col">
-
       {/* test change */}
       <div className="w-1/2 h-full  max-lg:w-full flex justify-center items-center rounded-md shadow-xl">
         <LineChart
@@ -21,7 +20,7 @@ export default function Price() {
         />
       </div>
       <div className="w-1/2 h-full  max-lg:w-full">
-        <div className="w-full h-10 flex justify-between gap-4  ">
+        <div className="w-full h-10 flex justify-between gap-4 mb-5 ">
           <button
             className={`w-1/2 font-yekan font-bold p-2  rounded-md ease-in-out duraion-200 transition-all cursor-pointer border border-gray-200  hover:border-main-hover ${sellOrBuy === "buy" ? "bg-main-select  border-main-hover" : ""}`}
             onClick={() => {
@@ -39,6 +38,12 @@ export default function Price() {
             فروش
           </button>
         </div>
+        <h1 className="font-yekan text-2xl font-bold mb-1">با تحلیل بازار طلا، هوشمندانه معامله کنید</h1>
+        <p className="font-yekan text-lg">
+          با رصد لحظه‌ای قیمت طلا، همیشه یک قدم جلوتر از بازار باشید؛ هر تغییر
+          را به فرصتی تبدیل کنید و با تصمیم‌های آگاهانه، آینده‌ای مطمئن و طلایی
+          برای خود بسازید.
+        </p>
       </div>
     </section>
   );
