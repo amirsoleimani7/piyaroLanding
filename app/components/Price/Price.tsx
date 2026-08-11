362ee8e535deefd66e8309d7e7c25f0b8e65dd16
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-11 16:45:19 +0330

    chore : test change to sync github config

--- app/components/Price/Price.tsx
+++ app/components/Price/Price.tsx
@@ -8,6 +8,8 @@ export default function Price() {
 
   return (
     <section className="w-[70%] max-md:w-[95%] rounded-md h-100 flex gap-5 max-lg:flex-col">
+
+      {/* test change */}
       <div className="w-1/2 h-full  max-lg:w-full flex justify-center items-center rounded-md shadow-xl">
         <LineChart
           xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
