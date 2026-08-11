232d3f6e09e6f4de570094da4d55144aa1d8688e
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-11 21:26:32 +0330

    style : simple change on buy and sell

--- app/components/Price/Price.tsx
+++ app/components/Price/Price.tsx
@@ -2,6 +2,7 @@
 
 import { LineChart } from "@mui/x-charts/LineChart";
 import { useState } from "react";
+import { ToPersianNumber } from "topersiannumber";
 
 import "./PriceStyle.css";
 
@@ -11,16 +12,19 @@ export default function Price() {
   return (
     <section className="w-[70%] max-md:w-[95%] rounded-md h-100 flex gap-5 max-lg:flex-col">
       {/* test change */}
+
       <div className="w-1/2 h-full  max-lg:w-full flex justify-center items-center rounded-md shadow-xl">
         <LineChart
           xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
           series={[
             {
               data: [2, 5.5, 2, 8.5, 1.5, 5],
+              area: true,
             },
           ]}
         />
       </div>
+
       <div className="w-1/2 h-full  max-lg:w-full flex flex-col">
         <div className="w-full h-10 flex justify-between gap-4 mb-5 ">
           <button
@@ -40,6 +44,7 @@ export default function Price() {
             فروش
           </button>
         </div>
+
         <h1 className="font-yekan text-2xl font-bold mb-1">
           با تحلیل بازار طلا، هوشمندانه معامله کنید
         </h1>
@@ -48,7 +53,9 @@ export default function Price() {
           را به فرصتی تبدیل کنید و با تصمیم‌های آگاهانه، آینده‌ای مطمئن و طلایی
           برای خود بسازید.
         </p>
+
         <div className="mt-auto mb-0 w-full ">
+          <div> {}</div>
           <div className="relative">
             <label htmlFor="price" className="font-yekan font-bold text-sm ">
               مبلغ پرداختی
@@ -61,7 +68,6 @@ export default function Price() {
               maxLength={30}
               pattern="[0-9]*"
               onChange={(e) => {
-                // Only allow digits
                 const value = e.target.value.replace(/\D/g, "");
                 e.target.value = value;
               }}
